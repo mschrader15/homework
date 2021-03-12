4e89324cb42b93aa5a173e2241b12f256b7639ed
mschrader15 <mcschrader@crimson.ua.edu>
2021-03-11 21:02:52 -0600

    added steepest descent w/ golden section search and newtons method

--- MA-521/computer-assignments/main.py
+++ MA-521/computer-assignments/main.py
@@ -1,8 +1,10 @@
+from math import sqrt
 from argparse import ArgumentParser
 from functools import wraps
 from time import process_time
 from datetime import datetime
 from numpy import array, matmul, add, subtract, mean, array_equal, zeros_like
+from numpy.linalg import inv, norm
 import sys
 
 # the setrecursionlimit function is
@@ -26,14 +28,16 @@ def timing(f):
         result = f(*args, **kw)
         te = process_time()
         return te - ts, result
+
     return wrap
+
 
 # class DescentMethod:
 
 
-class ConjugateGradient:
+class Optimizer:
     MAX_ITERATIONS = 10 ** 5
-    ERROR_TOLERANCE = 0.01
+    ERROR_TOLERANCE = 1e-5
 
     def __init__(self, input_dict):
         # unpacking the input dict:
@@ -42,46 +46,111 @@ class ConjugateGradient:
 
         # creating objects for all necessary variables in the G-G Method
         self.x_k = self.initial_x()
-        self.g_k = self.g_k_1_func()
-        self.d_k = -1 * self.g_k.copy()
-        self.d_k_Q_d_k = self.fast_d_k_Q_d_k()
+        self.g_k = None
+        self.d_k = None
+        self.d_k_Q_d_k = None
         self.zeros = zeros_like(self.g_k)
         self.alpha_k = None
         self.beta_k = None
-        self.error_tolerance = ConjugateGradient.ERROR_TOLERANCE ** 2
+        self.error_tolerance = Optimizer.ERROR_TOLERANCE ** 2
 
-    def g_k_1_func(self):
-        # g_0 = -gradient f(x_0) else gradient f(x_0)
-        # return subtract(matmul(self.Q, self.x_k.T), self.q) if k > 0 else subtract(self.q, matmul(self.Q, self.x_k.T))
-        return subtract(matmul(self.Q, self.x_k.T), self.q)
+    # def g_k_1_func(self):
+    #     # g_0 = -gradient f(x_0) else gradient f(x_0)
+    #     # return subtract(matmul(self.Q, self.x_k.T), self.q) if k > 0 else subtract(self.q, matmul(self.Q, self.x_k.T))
+    #     return add(matmul(self.Q, self.x_k.T), self.q)
+    #
+    # def fast_d_k_Q_d_k(self, ):
+    #     # this denominator term appears in both alpha_k and beta_k. Only compute once
+    #     return matmul(matmul(self.d_k.T, self.Q), self.d_k)
+    #
+    # def alpha_k_func(self, ):
+    #     return matmul(-1 * self.g_k, self.d_k) / self.d_k_Q_d_k
+    #
+    # def x_k_func(self, ):
+    #     return add(self.x_k, self.alpha_k * self.d_k)
+    #
+    # def d_k_func(self, ):
+    #     return add(-1 * self.g_k, self.beta_k * self.d_k)
+    #
+    # def beta_k_func(self, ):
+    #     return matmul(matmul(self.g_k.T, self.Q), self.d_k) / self.d_k_Q_d_k
 
-    def fast_d_k_Q_d_k(self, ):
-        # this denominator term appears in both alpha_k and beta_k. Only compute once
-        return matmul(matmul(self.d_k.T, self.Q), self.d_k)
-
-    def alpha_k_func(self, ):
-        return matmul(-1 * self.g_k, self.d_k) / self.d_k_Q_d_k
-
-    def x_k_func(self, ):
-        return add(self.x_k, self.alpha_k * self.d_k)
-
-    def d_k_func(self, ):
-        return add(-1 * self.g_k, self.beta_k * self.d_k)
-
-    def beta_k_func(self, ):
-        return matmul(matmul(self.g_k.T, self.Q), self.d_k) / self.d_k_Q_d_k
+    def gradient(self, x):
+        return matmul(self.Q, x) + self.q
 
     def initial_x(self, ):
         # return mean(array([self.ax, self.bx]), axis=0).astype(int)
         return zeros_like(self.ax)
 
     def check_stopping_conditions(self, k):
-        if k > ConjugateGradient.MAX_ITERATIONS:
+        if k > Optimizer.MAX_ITERATIONS:
             return "maximum iterations achieved", True
         elif array_equal(self.g_k, self.zeros):
             return "g_k = 0", True
         else:
             return None, False
+
+    def golden_section_search(self, x, g):
+        r = (sqrt(5) + 1) / 2
+        fn = lambda alpha: self.fn(x + alpha * g)
+        a = 0
+        b = self.fn(x)
+        c = b - (b - a) / r
+        d = a + (b - a) / r
+        while abs(b - a) > 0.001:
+            if fn(c) < fn(d):
+                b = d
+            else:
+                a = c
+            c = b - (b - a) / r
+            d = a + (b - a) / r
+        return (b + a) / 2
+
+    @timing
+    def steepest_descent(self):
+        stop = False
+        reason = None
+        self.x_k = self.initial_x()
+        k = 0
+        while not stop:
+            g_k = self.gradient(self.x_k)
+            a_k = self.golden_section_search(self.x_k, g_k)
+            self.x_k = self.x_k - a_k * g_k
+            if norm(g_k) < self.error_tolerance:
+                stop = True
+                reason = "solution found"
+            elif k > self.MAX_ITERATIONS:
+                stop = True
+                reason = "Max Iterations Reached"
+            k += 1
+        return reason
+
+    # @timing
+    # def accelerated_sd(self, ):
+    # # This is not working
+    #     stop = False
+    #     k = 0
+    #     reason = None
+    #     lambda_k = 0
+    #     x_tilda_k = zeros_like(self.ax)
+    #     self.x_k = zeros_like(self.ax)
+    #     while not stop:
+    #         g_k_1 = self.gradient(self.x_k)
+    #         lambda_k_1 = (1 + (1 + 4 * lambda_k)**(1/2)) / 2
+    #         alpha_k = (1 - lambda_k) / lambda_k_1
+    #         x_tilda_k_1 = self.x_k - alpha_k * g_k_1.T
+    #         self.x_k = (1 - alpha_k) * x_tilda_k_1 + alpha_k * x_tilda_k
+    #         if norm(g_k_1) < self.error_tolerance:
+    #             stop = True
+    #             reason = "solution found"
+    #         elif k > self.MAX_ITERATIONS:
+    #             stop = True
+    #             reason = "Max Iterations"
+    #         else:
+    #             lambda_k = lambda_k_1
+    #             x_tilda_k = x_tilda_k_1
+    #             k += 1
+    #     return reason
 
     @timing
     def cg(self, ):
@@ -89,29 +158,58 @@ class ConjugateGradient:
         stop = False
         k = 0
         reason = None
+        self.x_k = zeros_like(self.ax)
+        self.d_k = self.q - matmul(self.Q, self.x_k)
+        self.g_k = -1 * self.d_k
         while not stop:
-            # calculate d_k * Q * d_k.T first and only once, as it appears multiple times in subsequent functions
-            self.d_k_Q_d_k = self.fast_d_k_Q_d_k()
-            # calculate alpha (with last g_k)
-            self.alpha_k = self.alpha_k_func()
+            # calculate d_k.T * Q * d_k first and only once, as it appears multiple times in subsequent functions
+            self.d_k_Q_d_k = matmul(matmul(self.d_k.T, self.Q), self.d_k)
+            # calculate alpha
+            self.alpha_k = matmul(-1 * self.g_k.T, self.d_k) / self.d_k_Q_d_k
             # calculate x_k_1 with g_k
-            self.x_k = self.x_k_func()
+            x_k_1 = self.x_k + self.alpha_k * self.d_k
             # calculate g_k_1
-            self.g_k = self.g_k_1_func()
+            g_k_1 = matmul(self.Q, x_k_1) + self.q
             # calculate beta_k_1
-            self.beta_k = self.beta_k_func()
+            self.beta_k = matmul(matmul(g_k_1.T, self.Q), self.d_k) / self.d_k_Q_d_k
             # calculate d_k_1
-            self.d_k = self.d_k_func()
+            self.d_k = add(-1 * self.g_k, self.beta_k * self.d_k)
+            # pass x_k_1 and g_k_1 onto the next
+            self.x_k = x_k_1
+            self.g_k = g_k_1
             # check if stopping condition has been met
             reason, stop = self.check_stopping_conditions(k)
             k += 1
         return reason
 
-    def optimize(self, output_file_path):
-        # run the cg function and record the excution time
-        cpu_time, reason = self.cg()
+    @timing
+    def newton(self):
+        k = 0
+        stop = False
+        reason = None
+        self.x_k = zeros_like(self.ax)
+        Q_inv = inv(self.Q)
+        while not stop:
+            g_k_1 = matmul(self.Q, self.x_k) + self.q
+            x_k_1 = self.x_k - matmul(Q_inv, g_k_1.T)
+            if norm(x_k_1 - self.x_k) < self.error_tolerance:
+                stop = True
+                reason = "solution found"
+            elif k > self.MAX_ITERATIONS:
+                stop = True
+                reason = "Max Iterations"
+            else:
+                self.x_k = x_k_1
+            k += 1
+        return reason
+
+    def optimize(self, output_file_path, method):
+        # select the function based on user input
+        methods = {'cg': self.cg, 'newton': self.newton, 'steepest descent': self.steepest_descent}
+        # run the selected function and record the excution time
+        cpu_time, reason = methods[method]()
         # write the output of the function to the specified file path
-        self.write_output(cpu_time, output_file_path)
+        self.write_output(cpu_time, output_file_path, method)
 
     def fn(self, x):
         # evaluate the function at the specified point
@@ -125,15 +223,15 @@ class ConjugateGradient:
                 return False
         return True
 
-    def check_1st_order(self,):
+    def check_1st_order(self, ):
         # necessary 1st order condition
         # gradient(f(x*)) * d_k >= 0
         # g_k holds value from when cg() terminated, so it is = gradient(f(x*))
         return matmul(self.g_k, self.d_k) >= 0
 
-    def write_output(self, cpu_time, file_path):
+    def write_output(self, cpu_time, file_path, method):
         # create a list for the rows to write
-        write_strings = []
+        write_strings = [method]
         # these objects need to be restructured to print
         for item in [self.x_k, self.fn(self.x_k)]:
             # making the array more "pretty" to print
@@ -161,13 +259,15 @@ class ConjugateGradient:
 
         # write to file
         write_string = "\n".join([" = ".join([variable, value]) for variable, value
-                                  in zip(['x_star', 'f(x_star)', 'CPU time', 'inside bounds?', 'first order check'],
+                                  in zip(['method', 'x_star', 'f(x_star)', 'CPU time', 'inside bounds?',
+                                          'first order check'],
                                          write_strings)])
 
         # add in the time
-        write_string = datetime.now().strftime("%b %d %Y %H:%M:%S") + "\n" + write_string
+        write_string = "\n------------------------------------------------------------------\n" + \
+                       datetime.now().strftime("%b %d %Y %H:%M:%S") + "\n" + write_string
 
-        with open(file_path, 'w') as f:
+        with open(file_path, 'a+') as f:
             f.write(write_string)
 
 
@@ -231,7 +331,6 @@ def read_inputs(file_path):
 
 # the main entry point of the script
 if __name__ == "__main__":
-
     # reading the command line arguments
     cmd_args = parser.parse_args()
 
@@ -242,9 +341,9 @@ if __name__ == "__main__":
     check_inputs(inputs)
 
     # run algorithm of choice
-    if cmd_args.algorithm == 'CG':
-        # create the conjugate gradient object
-        cg = ConjugateGradient(input_dict=inputs)
+    # if cmd_args.algorithm == 'CG':
+    # create the conjugate gradient object
+    cg = Optimizer(input_dict=inputs)
 
-        # run the conjugate gradient algorithm
-        cg.optimize("output.txt")
+    # run the conjugate gradient algorithm
+    cg.optimize("output.txt", cmd_args.algorithm)
