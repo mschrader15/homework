e82bcd4aa4326dbe0a365b4284a45d4efe4daab8
Max Schrader <mcschrader@crimson.ua.edu>
2021-12-07 17:11:56 -0600

    Final Exam

--- fall-2021/me-591/hw_setup.py
+++ fall-2021/me-591/hw_setup.py
@@ -10,10 +10,10 @@ def print_msg_box(msg, indent=1, width=None, title=None):
     space = " " * indent
     if not width:
         width = max(map(len, lines))
-    box = f'╔{"═" * (width + indent * 2)}╗\n'  # upper_border
+    box = f'+{"-" * (width + indent * 2)}+\n'  # upper_border
     if title:
-        box += f'║{space}{title:<{width}}{space}║\n'  # title
-        box += f'║{space}{"-" * len(title):<{width}}{space}║\n'  # underscore
-    box += ''.join([f'║{space}{line:<{width}}{space}║\n' for line in lines])
-    box += f'╚{"═" * (width + indent * 2)}╝'  # lower_border
+        box += f'|{space}{title:<{width}}{space}|\n'  # title
+        box += f'|{space}{"-" * len(title):<{width}}{space}|\n'  # underscore
+    box += ''.join([f'|{space}{line:<{width}}{space}|\n' for line in lines])
+    box += f'+{"-" * (width + indent * 2)}+'  # lower_border
     print(box)
